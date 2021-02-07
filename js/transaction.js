146a47df4660b81532c11a84dcd699be8ad2ec18
AlexandraMarkova <a.markova@ukr.net>
2021-02-07 19:35:43 +0200

    фиксит возврат двух значений из промиса с помощью объекта

--- js/transaction.js
+++ js/transaction.js
@@ -5,14 +5,19 @@ const randomIntegerFromInterval = (min, max) => {
 };
 
 const makeTransaction = (transaction) => {
-     const delay = randomIntegerFromInterval(200, 500);  
-
-    return new Promise((resolve, reject) => {
+     
+ const delay = randomIntegerFromInterval(200, 500); 
+  return new Promise((resolve, reject) => {
+      
       setTimeout(() => {
           const canProcess = Math.random() > 0.3;
           
         if (canProcess) {
-          resolve(transaction.id, delay);
+          const deal = {
+            id: transaction.id,
+            time: delay
+          }
+          resolve(deal);
         } else {
           reject(transaction.id);
         }
@@ -20,7 +25,7 @@ const makeTransaction = (transaction) => {
     }); 
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
